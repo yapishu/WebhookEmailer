d6417730120e4afdb70a7e9202b5a401901333cd
reid <r.scoggin@gmail.com>
2021-11-23 14:49:07 -0600

    CSV parsing now modeled for actual Bridge export CSVs

--- webhookMailer.sh
+++ webhookMailer.sh
@@ -20,7 +20,7 @@
 #
 ### Edit these ⟀
 # Code for webhook to authenticate (20+ chars recommended)
-AUTH_CODE="put a nice long password here"
+AUTH_CODE="PlaceholderPassword"
 # Path to CSV
 CSV_FILE="TestPlanets.csv"
 SENDGRID_API_KEY="SG.placeholder"
@@ -31,6 +31,13 @@ SG_TEMPLATE="d-placeholder"
 # You can make one at https://mc.sendgrid.com/dynamic-templates
 
 ### Don't edit these ⟀
+# Standardize CSV from Bridge
+FIRST_LINE=`head -n 1 $CSV_FILE`
+TITLES="Number,Planet,Invite URL,Point,Ticket,Email,Timestamp"
+# Check if title row has all columns & replace if not
+if [[ "$FIRST_LINE" != "$TITLES" ]]; then
+sed -i "1s/.*/$TITLES/" $CSV_FILE;
+fi
 input=$1
 LOG_FILE="Transaction.log"
 TIMESTAMP=`date "+%Y.%m.%d-%H:%M:%S"`
@@ -44,15 +51,17 @@ AUTH_INPUT=`echo $input|jq .auth`
 AUTH_EXTRACT=`sed -e 's/^"//' -e 's/"$//'<<<"$AUTH_INPUT"`
 # Mark CSV entry as sold
 APPEND_STRING=`echo ,$EMAIL_EXTRACT,$TIMESTAMP`
-# Cut out lines with blank 3rd column
-CSV_CONTENT=`awk -F, '!length($3)' $CSV_FILE`
+# Cut out lines with blank 6th column
+CSV_CONTENT=`awk -F, '!length($6)' $CSV_FILE`
 # Snip the first one
 UNUSED_LINE=(${CSV_CONTENT[@]})
 # Snip out the @p, URL, and planet code
-UNUSED_CODE=`echo $UNUSED_LINE | cut -c 16-58`
-UNUSED_NAME=`echo $UNUSED_LINE | cut -c 1-14`
-CODE_TEXT=`echo $UNUSED_CODE | cut -c 17-44`
-LINE_NUM=`grep -Fn $UNUSED_NAME, $CSV_FILE | grep -Eo '[0-9]{1,4}'`
+UNUSED_CODE=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $3}'`
+UNUSED_NAME=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $2}'`
+CODE_TEXT=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $5}'`
+GET_LINE=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $1}'`
+LINE_NUM=$((GET_LINE++))
+# LINE_NUM=`grep -Fn $UNUSED_NAME, $CSV_FILE | grep -Eo '[0-9]{1,4}'`
 # JSON payloads for SendGrid
 # Use the dynamic_template_data vars in your template
 REQUEST_DATA='{ "from": {
@@ -88,14 +97,15 @@ BOUNCE_DATA='{ "from": {
 			[{"type": "text/plain", "value": "Email address validation failed, check logs"}]
 }';
 
+# Debug output
+                        echo $REQUEST_DATA
+                        echo "$CODE_TEXT"
+
 # Function to validate email address
 function isEmailValid() {
     regex="^([A-Za-z]+[A-Za-z0-9]*((\.|\-|\_)?[A-Za-z]+[A-Za-z0-9]*){1,})@(([A-Za-z]+[A-Za-z0-9]*)+((\.|\-|\_)?([A-Za-z]+[A-Za-z0-9]*)+){1,})+\.([A-Za-z]{2,})+"
     [[ "${1}" =~ $regex  ]]
 }
-
-# TODO:
-# Check whether CSV contains all column titles, append if not
 
 # Exclusive lock on executing script to avoid double-spends
 (
@@ -110,11 +120,11 @@ if [ "$AUTH_EXTRACT" = "$AUTH_CODE" ];then
 			curl -X "POST" "https://api.sendgrid.com/v3/mail/send" \
 				-H "Authorization: Bearer $SENDGRID_API_KEY" \
 				-H "Content-Type: application/json" \
-				-d "$REQUEST_DATA"
-			echo "$TIMESTAMP // Email sent to $EMAIL_EXTRACT" | tee -a "$LOG_FILE"
+				-d "$REQUEST_DATA" >> $LOG_FILE
+			echo  -e "\n$TIMESTAMP // Email sent to $EMAIL_EXTRACT" | tee -a "$LOG_FILE"
 # Append sale to CSV line and strip double-commas
-			sed -i ''${LINE_NUM}'s/$/'${APPEND_STRING}'/' $CSV_FILE
-            sed -i "s/,,/,/g" $CSV_FILE
+			sed -i ''${GET_LINE}'s/$/'${APPEND_STRING}'/' $CSV_FILE
+                        sed -i "s/,,/,/g" $CSV_FILE
 			echo "$TIMESTAMP // Transaction completed: $UNUSED_LINE,$EMAIL_EXTRACT,$TIMESTAMP" | tee -a "$LOG_FILE"
 
         else echo "$TIMESTAMP // $EMAIL_EXTRACT did not pass validation"  | tee -a "$LOG_FILE"
@@ -122,7 +132,7 @@ if [ "$AUTH_EXTRACT" = "$AUTH_CODE" ];then
 			curl -X "POST" "https://api.sendgrid.com/v3/mail/send" \
 				-H "Authorization: Bearer $SENDGRID_API_KEY" \
 				-H "Content-Type: application/json" \
-				-d "$BOUNCE_DATA"
+				-d "$BOUNCE_DATA" >> $LOG_FILE
 fi
     else echo "$TIMESTAMP // Failed validation: $AUTH_EXTRACT does not match $AUTH_CODE" | tee -a "$LOG_FILE"
 fi
