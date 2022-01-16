6e11c4c11b30a5a704f9e8f50edccf1eb320c94b
reid <r.scoggin@gmail.com>
2022-01-16 15:51:46 -0600

    v2: converted CSV parsing into sqlite database

--- webhookMailer.sh
+++ webhookMailer.sh
@@ -34,35 +34,42 @@ SG_TEMPLATE="d-placeholder"
 # You can make one at https://mc.sendgrid.com/dynamic-templates
 
 ### Don't edit these ⟀
-# Standardize CSV from Bridge
-FIRST_LINE=`head -n 1 $CSV_FILE`
-TITLES="Number,Planet,Invite URL,Point,Ticket,Email,Timestamp"
-# Check if title row has all columns & replace if not
-if [[ "$FIRST_LINE" != "$TITLES" ]]; then
-sed -i "1s/.*/$TITLES/" $CSV_FILE;
-fi
 input=$1
-LOG_FILE="Transaction.log"
-TIMESTAMP=`date "+%Y.%m.%d-%H:%M:%S"`
 # Snip email from input
 EMAIL_EXTRACT=`echo $input|jq -r .email`
 # Snip password from input
 AUTH_EXTRACT=`echo $input|jq -r .auth`
-# Mark CSV entry as sold
-APPEND_STRING=`echo ,$EMAIL_EXTRACT,$TIMESTAMP`
-# Cut out lines with blank 6th column
-CSV_CONTENT=`awk -F, '!length($6)' $CSV_FILE`
-# Snip the first one
-UNUSED_LINE=(${CSV_CONTENT[@]})
-# Snip out the @p, URL, and planet code
-UNUSED_CODE=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $3}'`
-UNUSED_NAME=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $2}'`
-CODE_TEXT=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $5}'`
-GET_LINE=`echo $UNUSED_LINE | awk -F',' 'NR==1{print $1}'`
-LINE_NUM=$((GET_LINE++))
-# LINE_NUM=`grep -Fn $UNUSED_NAME, $CSV_FILE | grep -Eo '[0-9]{1,4}'`
-# JSON payloads for SendGrid
-# Use the dynamic_template_data vars in your template
+LOG_FILE="Transaction.log"
+TIMESTAMP=`date "+%Y.%m.%d-%H.%M.%S"`
+# Check if the CSV has already been imported
+# If not, import the contents and mark as imported
+DB=db.sq3
+DB_SELECT="sqlite3 $DB 'SELECT"
+DB_UPDATE="sqlite3 $DB 'UPDATE"
+IMPORT_CHECK=`grep -q IMPORTED_FILE "${CSV_FILE}" ; echo $?`
+if [ "$IMPORT_CHECK" -eq "1" ]; then
+    # Import CSV to sqlite DB
+    # --import was added in 3.32
+    tail -n +2 "$CSV_FILE" > import.csv
+    sqlite3 $DB '.mode csv' 'import import.csv planets'
+    rm import.csv
+    # Change numbers to match rowid
+    eval "$DB_UPDATE planet SET Number = rowid;'"
+    # Mark CSV as imported
+    echo "IMPORTED_FILE" | tee -a $CSV_FILE
+    mv $CSV_FILE IMPORTED_${CSV_FILE}
+fi
+
+# Sqlite operation vars
+FIND_UNUSED="FROM planets WHERE Email is NULL LIMIT 1;'"
+APPEND_UNUSED="WHERE Email is NULL LIMIT 1;''"
+LINE_NUM=`eval "$DB_SELECT rowid $FIND_UNUSED"`
+UNUSED_CODE=`eval "$DB_SELECT \"Invite URL\" $FIND_UNUSED"`
+UNUSED_NAME=`eval "$DB_SELECT Planet $FIND_UNUSED"`
+CODE_TEXT=`eval "$DB_SELECT Ticket $FIND_UNUSED"`
+DB_COUNT=`eval "$DB_SELECT COUNT(*) FROM planets;'"`
+
+# Email data
 REQUEST_DATA='{ "from": {
                 "email": "'${FROM_EMAIL}'",
                 "name": "'${FROM_NAME}'"
@@ -95,6 +102,19 @@ BOUNCE_DATA='{ "from": {
 		"content":
 			[{"type": "text/plain", "value": "Email address validation failed, check logs"}]
 }';
+WARN_DATA='{ "from": {
+                "email": "'${FROM_EMAIL}'",
+                "name": "'${FROM_NAME}'"
+        },
+        "personalizations": [{
+                "to": [{
+                 "email": "'${FROM_EMAIL}'"
+        	     }],
+			}],
+        "subject": "WARNING: Final planet sold",
+		"content":
+			[{"type": "text/plain", "value": "No more planets available for sale, restock"}]
+}';
 
 # Debug output
                         echo $REQUEST_DATA
@@ -115,24 +135,37 @@ if [ "$AUTH_EXTRACT" = "$AUTH_CODE" ];then
        echo "$TIMESTAMP // Authentication succeeded: $AUTH_EXTRACT" | tee -a "$LOG_FILE"
 # Validate email
         if isEmailValid "$EMAIL_EXTRACT" ;then
-# Send email
+# Email is valid send email
 			curl -X "POST" "https://api.sendgrid.com/v3/mail/send" \
 				-H "Authorization: Bearer $SENDGRID_API_KEY" \
 				-H "Content-Type: application/json" \
 				-d "$REQUEST_DATA" >> $LOG_FILE
 			echo  -e "\n$TIMESTAMP // Email sent to $EMAIL_EXTRACT" | tee -a "$LOG_FILE"
-# Append sale to CSV line and strip double-commas
-			sed -i ''${GET_LINE}'s/$/'${APPEND_STRING}'/' $CSV_FILE
-                        sed -i "s/,,/,/g" $CSV_FILE
-			echo "$TIMESTAMP // Transaction completed: $UNUSED_LINE,$EMAIL_EXTRACT,$TIMESTAMP" | tee -a "$LOG_FILE"
+# Mark planet row as sold
+            eval "$DB_UPDATE SET Email = \"${EMAIL_EXTRACT}\", \
+            Timestamp = \"${TIMESTAMP}\" $APPEND_UNUSED"
 
+# Invalid email: mark to log
         else echo "$TIMESTAMP // $EMAIL_EXTRACT did not pass validation"  | tee -a "$LOG_FILE"
+
 # Email me if the recipient address is bad
 			curl -X "POST" "https://api.sendgrid.com/v3/mail/send" \
 				-H "Authorization: Bearer $SENDGRID_API_KEY" \
 				-H "Content-Type: application/json" \
 				-d "$BOUNCE_DATA" >> $LOG_FILE
-fi
+
+        fi
+
     else echo "$TIMESTAMP // Failed validation: $AUTH_EXTRACT does not match $AUTH_CODE" | tee -a "$LOG_FILE"
 fi
 ) 200>lock.file
+
+# Test whether any more planets are available
+
+if [ $LINE_NUM -eq $DB_COUNT ]; then
+# Email me if no more planets
+curl -X "POST" "https://api.sendgrid.com/v3/mail/send" \
+	-H "Authorization: Bearer $SENDGRID_API_KEY" \
+	-H "Content-Type: application/json" \
+	-d "$WARN_DATA" >> $LOG_FILE
+fi
