bc844082c09c846f543216cbe3c84f83ee3b47bc
reid <r.scoggin@gmail.com>
2022-01-17 20:17:51 -0600

    fixed file path

--- getStatus.sh
+++ getStatus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Easily query database status
 # $> ./getStatus.sh
-ROW_COUNT=`eval "sqlite3 planets.sq3 'SELECT COUNT(*) FROM planets;'"`
-AVAIL_ROW=`eval "sqlite3 planets.sq3 'SELECT Number FROM planets WHERE Email is NULL LIMIT 1;'"`
+ROW_COUNT=`eval "sqlite3 db.sq3 'SELECT COUNT(*) FROM planets;'"`
+AVAIL_ROW=`eval "sqlite3 db.sq3 'SELECT Number FROM planets WHERE Email is NULL LIMIT 1;'"`
 SOLD_COUNT=`echo $(($AVAIL_ROW-1))`
 REMAINING=`echo $(($ROW_COUNT-$SOLD_COUNT))`
 
@@ -20,8 +20,8 @@ echo $SOLD_COUNT
 echo
 echo "Most recently sold planet:"
 echo "-------------"
-eval "sqlite3 planets.sq3 'SELECT Planet FROM planets WHERE rowid = $SOLD_COUNT;'"
-eval "sqlite3 planets.sq3 'SELECT Timestamp FROM planets WHERE rowid = $SOLD_COUNT;'"
+eval "sqlite3 db.sq3 'SELECT Planet FROM planets WHERE rowid = $SOLD_COUNT;'"
+eval "sqlite3 db.sq3 'SELECT Timestamp FROM planets WHERE rowid = $SOLD_COUNT;'"
 echo
 echo "Number of remaining available planets:"
 echo "-------------"
